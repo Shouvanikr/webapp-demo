4187d72d514d12d39f42ffcf763dfff541ee5f5a
Shouvanik Roy <royshouvanik1@gmail.com>
2025-02-07 22:34:30 +0530

    Adding the fixes

--- src/main/java/com/edureka/webapp_demo/WebappDemoApplication.java
+++ src/main/java/com/edureka/webapp_demo/WebappDemoApplication.java
@@ -2,7 +2,10 @@ package com.edureka.webapp_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class WebappDemoApplication {
 
@@ -10,4 +13,8 @@ public class WebappDemoApplication {
 		SpringApplication.run(WebappDemoApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return String.format("<h1>Hello There, Welcome to my webapplication</h1>");
+	}
 }
--- src/test/java/com/edureka/webapp_demo/WebappDemoApplicationTests.java
+++ src/test/java/com/edureka/webapp_demo/WebappDemoApplicationTests.java
@@ -1,22 +1,13 @@
 package com.edureka.webapp_demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-@RestController
+@SpringBootTest
+class WebappDemoApplicationTests {
 
-public class WebappDemoApplicationTests {
-
-	public static void main(String[] args) {
-			SpringApplication.run(WebappDemoApplicationTests.class, args);
-		}
-
-		@GetMapping("/")
-		public String hello(){
-			return String.format("Hello there welocme to my web application");
-		}
+	@Test
+	void contextLoads() {
 	}
 
+}
